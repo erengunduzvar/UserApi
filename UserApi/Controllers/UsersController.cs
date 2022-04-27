7d6ad2b7113c33af9ae2ac4b79ec4811c3c7d286
erengunduzvar <70322002+erengunduzvar@users.noreply.github.com>
2022-04-27 18:21:12 +0300

    Add files via upload

---
+++ UserApi/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BookAPI.Repositories;
+using System.Threading.Tasks;
+using BookAPI.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsers() //tum kayıtlı kullanıcıları dondurme metodum
+        {
+            return await _userRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUsers(int id) //idye gore dondurme metodum
+        {
+            return await _userRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser([FromBody]User user) //Ekleme metodum
+        {
+            var newUser = await _userRepository.Create(user);
+            return CreatedAtAction(nameof(GetUsers), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutUsers(int id,[FromBody] User user) //uptade metodum
+        {
+            if(id != user.Id)
+            {
+                return BadRequest();
+            }
+            await _userRepository.Uptade(user);
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var bookToDelete = await _userRepository.Get(id);
+            if(bookToDelete != null)
+            {
+                await _userRepository.Delete(bookToDelete.Id);
+                return NoContent();
+            }
+            else
+                return NotFound();
+        }
+
+    }
+}
---
+++ UserApi/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            :base (options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
